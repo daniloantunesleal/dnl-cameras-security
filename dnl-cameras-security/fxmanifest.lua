8052e017e74a28e18f8320e85a53c0c2d3a2ab6e
Danilo Antunes Leal <96276849+daniloantunesleal@users.noreply.github.com>
2022-05-31 15:35:28 -0300

    Add files via upload

---
+++ dnl-cameras-security/fxmanifest.lua
@@ -0,0 +1,24 @@
+/*
+EM DESENVOLVIMENTO
+*/
+fx_version 'bodacious'
+game 'gta5'
+
+name 'Cameras Security'
+author 'Danilo Antunes Leal'
+version '1.0'
+/*
+DESENVOLVENDO
+shared_script {
+	'@vrp/lib/utils.lua',
+	'shared/*',
+	'config/*',
+}
+*/
+server_scripts {
+	'src/server.lua',
+}
+
+client_scripts {
+	'src/client.lua',
+}
